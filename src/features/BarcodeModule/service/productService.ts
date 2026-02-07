6765a607a719a5e285f8b49e5861766f3f2d8f00
francomats_ <francomats424@gmail.com>
2026-02-07 15:57:48 -0300

    cambios en el service

--- src/features/BarcodeModule/service/productService.ts
+++ src/features/BarcodeModule/service/productService.ts
@@ -3,16 +3,17 @@ import type { Product } from "../types/Product"
 
 export async function getProductByBarcode(
   barcode: string
-): Promise<Product | null> {
+): Promise<Product> {
   const res = await fetch(
-    `https://router.sgilibra.com:9443/api/products/${(barcode)}`,
+    `https://router.sgilibra.com:9443/api/products/${barcode}`,
     { credentials: "include" }
   )
 
+  const data = await res.json()
+
   if (!res.ok) {
-    if (res.status === 404) return null
-    throw new Error("Error al buscar producto")
+    throw new Error(data?.error || "Producto no encontrado")
   }
 
-  return res.json()
+  return data as Product
 }
